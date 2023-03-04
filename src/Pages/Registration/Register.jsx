3575bd760edc0bca30b78a85622245adeed1c728
Ankit Kumar <kumarankit.ak325@gmail.com>
2023-03-04 07:16:55 +0530

    making button

--- src/Pages/Registration/Register.jsx
+++ src/Pages/Registration/Register.jsx
@@ -1,21 +1,20 @@
-import { Container } from "@mui/material";
+import { Button, Container, TextField } from "@mui/material";
 import React from "react";
 import FormInput from "../../components/FormInput/FormInput";
 
 function Register() {
   return (
-    <Container maxWidth='xl' sx={{
-      display:'flex',
-      justifyContent:'center',
-      alignItems:'center',
-      height:'100vh',
-      flexDirection:'column',
-      backgroundColor:'#93BFCF',
-    }} >
-      <FormInput label='Username' />
-      <FormInput label='Email' />
-      <FormInput label='Password' />
-      <FormInput label='Confirm Password' />
+    <Container
+      maxWidth="xl"
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#93BFCF",
+      }}
+    >
+      <FormInput />
     </Container>
   );
 }
--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,12 +1,121 @@
-import {Box, TextField } from "@mui/material";
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
 import React from "react";
+import { useFormik } from "formik";
+import { Link } from "react-router-dom";
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
 
-function FormInput(props) {
+function FormInput() {
+  const { values, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    onSubmit: (data) => {
+      console.log(data);
+    },
+  });
   return (
-    <Box sx={{
-      backgroundColor:'#6096B4'
-    }}>
-      <TextField label={props.label} />
+    <Box
+      sx={{
+        backgroundColor: "#fff",
+        padding: 4,
+        borderRadius: 2,
+        textAlign: "center",
+        width: 380,
+      }}
+    >
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Typography
+          variant="h1"
+          sx={{
+            marginBottom: "20px",
+            fontSize: 25,
+            fontWeight: 700,
+          }}
+        >
+          Register
+        </Typography>
+        <TextField
+          onChange={handleChange}
+          type="text"
+          value={values.username}
+          name="username"
+          required
+          label="Username"
+          sx={{
+            marginBottom: "15px",
+          }}
+        />
+        <TextField
+          onChange={handleChange}
+          type="email"
+          value={values.email}
+          name="email"
+          required
+          label="Email"
+          sx={{
+            marginBottom: "15px",
+          }}
+        />
+        <TextField
+          onChange={handleChange}
+          type="password"
+          value={values.password}
+          name="password"
+          required
+          label="Password"
+          sx={{
+            marginBottom: "15px",
+          }}
+        />
+        <TextField
+          onChange={handleChange}
+          type="password"
+          value={values.confirmPassword}
+          name="confirmPassword"
+          required
+          label="Confirm Password"
+          sx={{
+            marginBottom: "15px",
+          }}
+        />
+        <Button
+          sx={{ height: 50, fontWeight: 700, fontSize: 15 }}
+          type="submit"
+          variant="contained"
+        >
+          Submit
+        </Button>
+      </form>
+      <Typography mt={1}>
+        Already have an account?{" "}
+        <Link
+          style={{
+            textDecoration: "none",
+          }}
+          to="/login"
+        >
+          Sign In
+        </Link>
+      </Typography>
+      <Typography
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent="center"
+      >
+        {" "}
+        <FacebookRoundedIcon />
+        Login With Facebook
+      </Typography>
+      <Typography>Login With Google</Typography>
     </Box>
   );
 }
